19028a9d7a4fd5f584b223b256da46d220fc271a
LocTM <tranminhloc.anhk@gmail.com>
2023-11-09 09:19:21 +0700

    first

---
+++ index.js
@@ -0,0 +1,20 @@
+// Điểm trung bình 3 môn học
+// a = prompt("Điểm vật lý: ");
+// b = prompt("Điểm hóa học: ");
+// c = prompt("Điểm sinh học: ");
+// alert("Điểm trung bình: " + (parseInt(a) + parseInt(b) + parseInt(c)) / 3);
+
+// Đổi độ C sang độ F
+// A = prompt("Nhập độ C: ");
+// B = (parseInt(A) * 9) / 5 + 32;
+// alert("Độ F: " + B);
+
+// Tính diện tích hình tròn
+// R = prompt("Nhập bán kính: ");
+// S = parseInt(R) * parseInt(R) * 3.14;
+// alert("Diện tích hình tròn: " + S);
+
+// Tính chu vi hình tròn
+// R = prompt("Nhập bán kính: ");
+// C = parseInt(R) * 2 * 3.14;
+// alert("Chu vi hình tròn: " + C);
